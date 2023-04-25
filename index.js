60ab2bbd3737148ca749e21598c34c1783c2e2f3
Liam DeWitt <mugzy550@gmail.com>
2023-04-25 16:22:42 -0400

    require mongoose connect mongoose create error handling when connecting to database app.get makecampground require model

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const mongoose = require("mongoose");
+
+const Campground = require("./models/campground");
+
+mongoose.connect("mongodb://localhost:27017/yelp-camp");
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Database connected");
+});
 
 app.set('view engine', 'ejs');
 app.set('views',path.join(__dirname, 'views'))
@@ -9,6 +20,12 @@ app.get('/', (req,res)=>{
     res.render('home')
 })
 
+app.get("/makecampground", async (req, res) => {
+    const camp = new Campground({title: 'My Backyard', description: 'cheap camping!'});
+    await camp.save();
+    res.send(camp)
+  });
+  
 app.listen(4000, ()=>{
     console.log('Serving on port 4000')
 })
