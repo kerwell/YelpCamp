9cdf4dc24fccea0c99382ea5b374abf2f53bf28b
Liam DeWitt <mugzy550@gmail.com>
2023-04-25 20:18:54 -0400

    make dynamic function that chooses a random number dependent on array.length
    make seedDB function that deletes everything in the campground
    collection
    
    make function create a new location and descriptors based
    off the sample function
    
    make seedDB function close connection after executing

--- seeds/index.js
+++ seeds/index.js
@@ -9,3 +9,23 @@ db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
   console.log("Database connected");
 });
+
+const sample = (array) => {
+    return array[Math.floor(Math.random() * array.length)];
+  };
+  
+  const seedDB = async () => {
+    await Campground.deleteMany({});
+    for (let i = 0; i < 50; i++) {
+      const newCamp = new Campground({
+        location: `${sample(cities).city}, ${sample(cities).state}`,
+        title: `${sample(descriptors)} ${sample(places)}`,
+      });
+      await newCamp.save();
+    }
+  };
+
+
+seedDB().then(() => {
+  mongoose.connection.close();
+});
