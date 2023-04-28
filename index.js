09a166ded5c31e60f11a4ea8ff968b9b1c41b4ca
Liam DeWitt <mugzy550@gmail.com>
2023-04-27 20:15:15 -0400

    require method-override make app.use middleware for method-override

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
+const methodOverride = require('method-override');
+
 
 const Campground = require("./models/campground");
 
@@ -17,6 +19,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
 app.get("/", (req, res) => {
   res.render("home");
