c191e6c929196356c97b13461bb099e322aecab5
Liam DeWitt <mugzy550@gmail.com>
2023-05-02 06:25:23 -0400

    add images to seedDb function add description to seedDb function add price to seedDb function

--- seeds/index.js
+++ seeds/index.js
@@ -17,9 +17,14 @@ const sample = (array) => {
   const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
+      const price = Math.floor(Math.random()* 20) + 10;
       const newCamp = new Campground({
+        // location: `${cities[random1000].city}, ${cities[random1000].state}`,
         location: `${sample(cities).city}, ${sample(cities).state}`,
         title: `${sample(descriptors)} ${sample(places)}`,
+        image: `https://picsum.photos/500/400`,
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere omnis asperiores sequi natus maxime incidunt sapiente! Tenetur cumque nemo aperiam ipsa tempore? Maxime eligendi consequuntur assumenda quis sequi quod veniam!',
+        price
       });
       await newCamp.save();
     }
