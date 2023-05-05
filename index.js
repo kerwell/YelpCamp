a142df0033d17a0fe73d4ff0e895e596a58c48ce
Liam DeWitt <mugzy550@gmail.com>
2023-05-05 04:46:13 -0400

    require catchAsync

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 const Campground = require("./models/campground");
+const catchAsync = require('./utilities/catchAsync');
 
 const app = express();
 
