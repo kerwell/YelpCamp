85e8aaa12ea5921d88f78dcb486b0ef1f1c4d8e6
Liam DeWitt <mugzy550@gmail.com>
2023-05-08 08:57:56 -0400

    let there be schemas.js

---
+++ schemas.js
@@ -0,0 +1,11 @@
+const Joi = require("joi");
+// if(!req.body.campground) throw new ExpressError('Invalid Campground Data',400)
+module.exports.campgroundSchema = Joi.object({
+  campground: Joi.object({
+    title: Joi.string().required(),
+    price: Joi.number().required().min(0),
+    image: Joi.string().required(),
+    location: Joi.string().required(),
+    description: Joi.string().required(),
+  }).required(),
+});
