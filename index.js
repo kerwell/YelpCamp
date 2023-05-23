19f8fc9b0535fef7e599495b267721319d09599f
Liam DeWitt <mugzy550@gmail.com>
2023-05-23 14:00:09 -0400

    make an app.post route for '/campgrounds/:id/reviews'

--- index.js
+++ index.js
@@ -80,6 +80,10 @@ app.delete("/campgrounds/:id", catchAsync(async (req,res)=>{
     res.redirect('/campgrounds');
   }));
 
+app.post("/campgrounds/:id/reviews", catchAsync(async (req,res)=>{
+  res.send('You made it!')
+}));
+
 app.all('*',(req,res,next)=>{
   next(new ExpressError('Page Not Found...', 404))
 })
